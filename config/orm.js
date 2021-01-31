f94a170fae17c761d3e2b833a21fa88f3ab89271
Coleman Buffa <coleman.buffa@gmail.com>
2021-01-30 23:28:30 -0800

    Add SQL update functionality to model segment.

--- config/orm.js
+++ config/orm.js
@@ -14,6 +14,25 @@ function printQuestionMarks(num) {
 
   return arr.toString();
 }
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      arr.push(key + "=" + value);
+    }
+  }
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
 
 const orm = {
 
@@ -43,8 +62,18 @@ const orm = {
 		});
 	},
 
-	updateOne: function() {
+	updateOne: function(table, objColVals, condition, cb) {
+		let queryStr = "UPDATE " + table;
+		queryStr += " SET ";
+		queryStr += objToSql(objColVals);
+		queryStr += " WHERE ";
+		queryStr += condition;
 
+		console.log(queryString);
+		connection.query(queryStr, function(err, result) {
+			if (err) throw err;
+			cb(result);
+		});
 	}
 }
 
