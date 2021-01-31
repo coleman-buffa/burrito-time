d9319f2120a6cc329f4731a4b358b5497c647740
Coleman Buffa <coleman.buffa@gmail.com>
2021-01-30 23:34:02 -0800

    Add SQL update functionality to control segment.

--- controllers/burrito_controller.js
+++ controllers/burrito_controller.js
@@ -1,26 +1,42 @@
-const express  = require('express');
+const express = require('express');
 //Import the model to use its database functions
 const burrito = require('../models/burrito.js');
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-	burrito.selectAll(function(data) {
+router.get('/', function (req, res) {
+	burrito.selectAll(function (data) {
 		let hbsObject = {
 			burritos: data
 		};
 		console.log(hbsObject);
-		res.render('index', hbsObject);		
+		res.render('index', hbsObject);
 	});
 });
 
-router.post('/api/burritos', function(req, res) {
+router.post('/api/burritos', function (req, res) {
 	burrito.insertOne([
 		"burrito_name", "devoured"
 	], [
 		req.body.burrito_name, req.body.devoured
-	], function(result) {
+	], function (result) {
 		res.json({ id: result.insertId });
+	});
+});
+
+router.put("/api/burritos/:id", function (req, res) {
+	let condition = "id = " + req.params.id;
+
+	console.log("condition", condition);
+
+	burrito.updateOne({
+		devoured: req.body.devoured
+	}, condition, function (result) {
+		if (result.changedRows == 0) {
+			return res.status(404).end();
+		} else {
+			res.status(200).end();
+		}
 	});
 });
 
